957d47f32d668c54d9336021d5db038d08fa4886
Gerardo Mecott <gerardomecott@outlook.com>
2026-04-09 19:01:46 -0600

    Modificando css y html de profile user

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import { fileURLToPath } from "node:url";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+        "@components": fileURLToPath(
+          new URL("./src/components", import.meta.url),
+        ),
+        "@layouts": fileURLToPath(new URL("./src/layouts", import.meta.url)),
+        "@assets": fileURLToPath(new URL("./src/assets/*", import.meta.url))
+      },
+    },
+  },
+});
